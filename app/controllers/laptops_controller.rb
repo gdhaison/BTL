b249e3d2fb4b7bdbf8de527c5aad0808996825a4
gdhaison <vuhaisonhb456@gmail.com>
2019-04-02 13:53:20 +0700

    merge

--- app/controllers/laptops_controller.rb
+++ app/controllers/laptops_controller.rb
@@ -17,6 +17,6 @@ def create
 end
   private
   def laptop_params
-    params.require(:laptop).permit(:name, :descripetion, :price,:image1,:image2,:image3,:ram,:monitor,:weight)
+    params.require(:laptop).permit(:name, :descripetion, :price,:image1,:image2,:image3,:ram,:monitor,:weight,:company)
   end
 end
