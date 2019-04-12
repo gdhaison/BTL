d1d2ee4f698e16b77690bea2bcfb09cef953aa3c
gdhaison <vuhaisonhb456@gmail.com>
2019-04-12 09:05:07 +0700

    a

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'laptops/new'
   get "static_pages/home"
   root "static_pages#home"
+  get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
   post "/signup",to: "users#create"
   get "/edit",to: "users#edit"
